2449dfa20d35ac600f62876b608006cac9e3bd8f
Anderson Ferreira <ander_sampaio@yahoo.com.br>
2023-10-25 21:39:54 -0300

    Trabalhando com Template Strings

---
+++ index.js
@@ -0,0 +1,17 @@
+
+let numA = 4
+let numB = 5
+
+let soma = `A soma de ${numA} e ${numB} é ${numA + numB}`
+console.log(soma)
+
+let subtracao = `A subtração de ${numA} e ${numB} é ${numA - numB}`
+console.log(subtracao)
+
+let multiplicacao = `A multiplicação de ${numA} e ${numB} é ${numA * numB}`
+console.log(multiplicacao)
+
+let divisao = `A divisão de ${numA} e ${numB} é ${numA / numB}`
+console.log(divisao)
+
+
